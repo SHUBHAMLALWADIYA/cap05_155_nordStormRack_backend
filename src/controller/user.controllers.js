4c56c43fb9fae1fe4496ae1b9fb0bb430accf654
unknown <lalwadiyashubham123@gmail.com>
2024-02-04 23:30:46 +0530

    nodemailer is not working so we just removed it

--- src/controller/user.controllers.js
+++ src/controller/user.controllers.js
@@ -4,7 +4,6 @@ const cookieParser=require("cookie-parser")
 const bcrypt=require("bcrypt");
 const jwt=require("jsonwebtoken");
 const dotenv=require("dotenv");
-const { sendMail } = require("../config/nodemailer");
 dotenv.config();
 
 const acc_secretKey=process.env.ACCESSSTOKEN_SECRETEKEY
@@ -15,18 +14,6 @@ const ref_secretKey=process.env.REFRESHTOKEN_SECRETEKEY
 const signup=async(req,res)=>{
    
 
-    //email
-    // yourApp.js
-const sendEmail = require('./emailService');
-
-// Example: Sending a welcome email
-
-const welcomeSubject = "Welcome and Shubham Lalwadiya's web-site  just follow me in linkdin";
-const welcomeText = 'Thank you for registering . We look forward to having you as a member!';
-//
-
-
-    
     const passwordRegex = /^(?=.*[A-Z])(?=.*[@$!%*?&]).{8,}/;
     let {email,username,pass}=req.body
     try {
@@ -47,16 +34,7 @@ const welcomeText = 'Thank you for registering . We look forward to having you a
                 await user.save()//replace with create
                 console.log(req.body)
 
-                sendMail(email, welcomeSubject, welcomeText)
-  .then(() => {
-    console.log('Email sent successfully!');
-  })
-  .catch((error) => {
-    console.error('Error sending email:', error);
-  });
-
-
-
+               
                 return res.status(201).send({msg:"New user has been successfully created account"})
             }
         })  
@@ -125,62 +103,5 @@ console.log(req.cookies)
 };
 
 
-// const forgetPass=async (req, res) => {
-//     try {
-        
-//         const { email } = req.body;
-  
-//     // Find the user by email
-//     const user =await UserModel.find({email:email});
-  
-//     // Check if the user exists
-//     if (!user) {
-//       return res.status(404).json({ error: 'User not found' });
-//     }
-  
-//     // Generate a new OTP for password reset
-//     const newOtp = Math.floor(100000 + Math.random() * 900000);
-  
-//     // Update user's OTP in the data (you might want to update it in the database)
-//     user.otp = newOtp;
-  
-//     // Send OTP to the user's email
-//     sendOtpEmail(email, newOtp);
-  
-//     res.json({ message: 'OTP sent to your email for password reset.' });
-  
-  
-//     } catch (error) {
-//         res.send({msg:"we are in forget password functinality",error:error.message})
-//     }
-// }
-
-
-//   // Function to send OTP email
-//   function sendOtpEmail(email, otp,pass) {
-//     const transporter = nodemailer.createTransport({
-//       service: 'gmail',
-//       auth: {
-//         user: "shubhamlalwadiya@gmail.com",
-//         pass: "Shubham@230201",
-//       },
-//     });
-    
-  
-//     const mailOptions = {
-//       from: "shubhamlalwadiya@gmail.com",
-//       to: email,
-//       subject: 'Your OTP for NordStackRack',
-//       text: `Your OTP for NordStackRack is: ${otp}`,
-//     };
-  
-//     transporter.sendMail(mailOptions, (error, info) => {
-//       if (error) {
-//         console.error('Error sending email:', error);
-//       } else {
-//         console.log('Email sent:', info.response);
-//       }
-//     });
-//   }
 
 module.exports={login,signup,logout}
