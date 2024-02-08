eca76725cb329b944599d7b727257c5fb0815aad
unknown <lalwadiyashubham123@gmail.com>
2024-02-08 12:03:29 +0530

    adding link in cors

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(cors({
     origin:[
         "https://cap05155nordstormrackbackend-production.up.railway.app",
         "http://127.0.0.1:5173",
-       "nordstormstackcap05155.netlify.app"
+       "https://nordstormstackcap05155.netlify.app"
     ],
     credentials:true,
 }
